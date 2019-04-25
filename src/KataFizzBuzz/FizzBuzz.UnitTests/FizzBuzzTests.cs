1f2f1ae5c0f22967e5fcfc897b95eced3bec49cb
Roland van Oers <rvoers@vaa.com>
2019-04-25 11:52:59 +0200

    Fixed

--- src/KataFizzBuzz/FizzBuzz.UnitTests/FizzBuzzTests.cs
+++ src/KataFizzBuzz/FizzBuzz.UnitTests/FizzBuzzTests.cs
@@ -7,6 +7,19 @@ namespace FizzBuzz.UnitTests
     {
         [Fact]
         public void ReturnString() =>
-            new FizzBuzz().GetStringForNumber().Should().BeOfType<string>();
+            new FizzBuzz().GetStringForNumber(0).Should().BeOfType<string>();
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(10, "Buzz")]
+        [InlineData(30, "FizzBuzz")]
+        public void ReturnNumber(int input, string expected)
+        {
+            new FizzBuzz().GetStringForNumber(input).Should().Be(expected);
+        }
     }
 }
--- src/KataFizzBuzz/FizzBuzz/FIzzBuzz.cs
+++ src/KataFizzBuzz/FizzBuzz/FIzzBuzz.cs
@@ -4,6 +4,35 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
-        public string GetStringForNumber() => throw new NotImplementedException();
+        public string GetStringForNumber(int number)
+        {
+            string result = null;
+            if (number % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            return result ?? number.ToString();
+        }
+
+        public string GetStringForNumber1(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
     }
 }

81fe2a242f58fa2e8f3802a959873c984d52520b
Bob Klaase <bklaase@vaa.com>
2019-04-25 13:28:29 +0200

    completed tests and implementation of GetStringForNumber function

--- src/KataFizzBuzz/FizzBuzz.UnitTests/FizzBuzzTests.cs
+++ src/KataFizzBuzz/FizzBuzz.UnitTests/FizzBuzzTests.cs
@@ -7,6 +7,38 @@ namespace FizzBuzz.UnitTests
     {
         [Fact]
         public void ReturnString() =>
-            new FizzBuzz().GetStringForNumber().Should().BeOfType<string>();
+            new FizzBuzz().GetStringForNumber(3).Should().BeOfType<string>();
+
+        [Theory]
+        [InlineData(3, "Fizz")]
+        [InlineData(6, "Fizz")]
+        public void ReturnFizzOnMultipleOfThree(int number, string result) =>
+            new FizzBuzz().GetStringForNumber(number).Should().Be(result);
+
+        [Theory]
+        [InlineData(5, "Buzz")]
+        [InlineData(10, "Buzz")]
+        public void ReturnBuzzOnMultipleOfFive(int number, string result) =>
+            new FizzBuzz().GetStringForNumber(number).Should().Be(result);
+
+        [Theory]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(30, "FizzBuzz")]
+        public void ReturnBuzzOnMultipleOfFifteen(int number, string result) =>
+            new FizzBuzz().GetStringForNumber(number).Should().Be(result);
+
+        [Theory]
+        [InlineData(11, "11")]
+        [InlineData(17, "17")]
+        public void ReturnStringRepresentationOfNumberWhenNotFizzBuzzNumber(int number, string result) =>
+            new FizzBuzz().GetStringForNumber(number).Should().Be(result);
+
+        [Theory]
+        [InlineData(7, "FizzBuzzBang")]
+        [InlineData(14, "FizzBuzzBang")]
+        [InlineData(28, "FizzBuzzBang")]
+        public void ReturnFizzBuzzBangOnMultipleOfSeven(int number, string result) =>
+            new FizzBuzz().GetStringForNumber(number).Should().Be(result);
     }
 }
+
--- src/KataFizzBuzz/FizzBuzz/FIzzBuzz.cs
+++ src/KataFizzBuzz/FizzBuzz/FIzzBuzz.cs
@@ -4,6 +4,20 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
-        public string GetStringForNumber() => throw new NotImplementedException();
+        public string GetStringForNumber(int number)
+        {
+            if (number % 7 == 0)
+                return "FizzBuzzBang";
+            else if (number % 15 == 0)
+                return "FizzBuzz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else
+                return number.ToString();
+        }
+
     }
 }
+
